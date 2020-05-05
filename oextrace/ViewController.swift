5a59d8c2ec2b89c0ebd60c5d7dfc0a30a248d4c2
Alexander Ivanov <alexander.n.ivanov@yandex.ru>
2020-05-05 16:17:00 +0300

    Initial Commit

---
+++ oextrace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  oextrace
+//
+//  Created by Alexander Ivanov on 05.05.2020.
+//  Copyright © 2020 OpenExposureTrace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
